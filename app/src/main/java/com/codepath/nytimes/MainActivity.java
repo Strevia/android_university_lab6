46f0f6b56fb577c436616ace88a9426e8c95d149
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2022-10-20 16:50:58 -0400

    Frament and main activity

--- app/src/main/java/com/codepath/nytimes/MainActivity.java
+++ app/src/main/java/com/codepath/nytimes/MainActivity.java
@@ -1,8 +1,17 @@
 package com.codepath.nytimes;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.codepath.nytimes.ui.HomeFragment;
+import com.codepath.nytimes.ui.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // define your fragments here
+        final Fragment fragment1 = new HomeFragment();
+        final Fragment fragment2 = new SettingsFragment();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        // handle navigation selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.action_home:
+                                fragment = fragment1;
+                                break;
+                            case R.id.action_settings:
+                                fragment = fragment2;
+                                break;
+//                            case R.id.action_music:
+//                            default:
+//                                fragment = fragment3;
+//                                break;
+                        }
+                        fragmentManager.beginTransaction().replace(R.id.frame_layout, fragment).commit();
+                        return true;
+                    }
+                });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
---
+++ app/src/main/java/com/codepath/nytimes/ui/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.codepath.nytimes.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.codepath.nytimes.R
+
+class HomeFragment: Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home,container,false)
+    }
+}
