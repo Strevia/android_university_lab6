e78590129b9c74e8f4bad46635d0a9d89b799907
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2022-10-20 17:06:57 -0400

    Buggy

--- app/src/main/java/com/codepath/nytimes/ui/ArticleResultFragment.kt
+++ app/src/main/java/com/codepath/nytimes/ui/ArticleResultFragment.kt
@@ -1,12 +1,12 @@
 package com.codepath.nytimes.ui
 
+import android.R
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.MenuItemCompat
 import androidx.fragment.app.Fragment
 import com.codepath.nytimes.R
+
 
 class ArticleResultFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -18,12 +18,33 @@ class ArticleResultFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_article_result,container,false)
+        return inflater.inflate(com.codepath.nytimes.R.layout.fragment_article_result,container,false)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setHasOptionsMenu(true)
+    }
+    fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = getMenuInflater()
+        inflater.inflate(com.codepath.nytimes.R.menu.main, menu)
+        val searchItem: MenuItem = menu.findItem(com.codepath.nytimes.R.id.action_search)
+        val searchView: SearchView = MenuItemCompat.getActionView(searchItem) as SearchView
+        searchView.setOnQueryTextListener(object : OnQueryTextListener() {
+            fun onQueryTextSubmit(query: String?): Boolean {
+                // perform query here
+
+                // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
+                // see https://code.google.com/p/android/issues/detail?id=24599
+                searchView.clearFocus()
+                return true
+            }
+
+            fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 }
